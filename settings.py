fd0a92bd925b47d8489fa26cc900c891edf1ab2b
Impish <Impish@orfun.ru>
2016-01-10 15:24:44 +0500

    settings

--- settings.py
+++ settings.py
@@ -1,12 +1,18 @@
-USER_CLASS = 'manage.models.Staff'
-password_salt = 'G98uasfjo9!j0p9kfdlkj-089'
-
-#   postgresql
-db = {
-        'driver': 'postgresql+psycopg2',
-        'bd_name': 'ech',
-        'username': 'postgres',
-        'password': '1',
-        'host': '127.0.0.1',
-        'port': '',
-    }
+# db = {
+#         'driver': 'postgresql+psycopg2',
+#         'bd_name': '',
+#         'username': '',
+#         'password': '',
+#         'host': '',
+#         'port': '',
+#     }
+#
+# tornado_settings = {
+#     'login_url': "/manage",
+#     'cookie_secret': "",
+#     'xsrf_cookies': False, # !!
+#     'autoreload': False,
+#     'debug': False,
+#     'media': 'media',
+#     'password_salt': '',
+# }
