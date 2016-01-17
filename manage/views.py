3167e05df7ffaafa3280dc9a3148b40ef7d7ecfa
Impish <Impish@orfun.ru>
2016-01-17 21:41:41 +0500

    аменд же

--- manage/views.py
+++ manage/views.py
@@ -17,9 +17,8 @@ class ManageHandler(BoardDataMixin, TemplateHandler):
     template_name = 'manage.html'
 
     def post(self):
-        user = self.db.query(Staff).filter(Staff.name == self.get_argument("login"),
-                                           Staff.password == self.get_argument('password')).first()
-        if user is None:
+        user = self.db.query(Staff).filter(Staff.name == self.get_argument("login")).first()
+        if user.password == self.get_argument('password', None) is None:
             return self.get()
         self.set_secure_cookie("user_id", '%d' % user.id)
         self.redirect("/manage")
