da47a8df5421c6230d4136b67856aeadd3d9fbf9
Impish <Impish@orfun.ru>
2016-02-01 16:02:38 +0500

    order boards

--- manage/models.py
+++ manage/models.py
@@ -190,7 +190,7 @@ class Board(Base, SessionMixin):
     thread_tail = Column(Integer, default=5, label=u'Хвост треда(сообщений на странице)')
     captcha = Column(BOOLEAN, default=False, label=u'Капча')
     section = relationship('Section',
-                           backref=backref('boards', lazy="subquery", order_by="Message.gid",))
+                           backref=backref('boards', lazy="subquery", order_by="Board.dir",))
     section_id = Column(Integer, ForeignKey('section.id'))
 
     available_from = Column(String, label=u'(Часы доступа(мск)) Доступна с ', nullable=True)
